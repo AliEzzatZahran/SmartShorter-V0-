eb22760d8ed3a14a0e20de4ada0c4e2da5312480
AliEzzatZahran <alilolllzahranali@gmail.com>
2018-10-02 12:12:48 +0200

    database configration

--- project/config.py
+++ project/config.py
@@ -13,7 +13,5 @@ HOST ='127.0.0.1'
 #Database
 MONGO_URI = 'mongodb://localhost:27017/'
 MONGO_PORT = 27017
-MONGO_USERNAME = ''
-MONGO_PASSWORD = ''
 MONGO_TABLE_NAME='shortlink'
 MONGO_DATABASE_NAME='shortlinks'
